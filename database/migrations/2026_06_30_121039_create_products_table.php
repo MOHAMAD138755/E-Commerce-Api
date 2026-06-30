be178d96d4b975fd24bf0f550e1ceea14d23fb7d
mohamadtorki <mohamadprograming123@gmail.com>
2026-06-30 15:48:16 +0330

    refactor(v1): update product migration fields

--- database/migrations/2026_06_30_121039_create_products_table.php
+++ database/migrations/2026_06_30_121039_create_products_table.php
@@ -14,10 +14,11 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->unsignedBigInteger('price');
             $table->integer('stock')->default(0);
-            $table->text('description');
+            $table->text('description')->nullable();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
